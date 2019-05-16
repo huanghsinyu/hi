668e3ce1d78046c008ab024565a83c28a4a8490f
amber <hsinyuhuang2@google.com>
2019-05-16 23:24:22 +0800

    add new file

---
+++ 123.py
@@ -0,0 +1,18 @@
+print(100)
+print(100000)
+x=5
+print(x)
+y=8
+print(x,y)
+z='jgdsrt'
+print(x,y,z)
+price=10
+pen=0.38
+rain=True
+name='amber'
+print(price,pen,rain,name)
+name=input('請輸入名字：')
+print('hi',name)
+input('請輸入身高: ')
+input('請輸入體重: ')
+
